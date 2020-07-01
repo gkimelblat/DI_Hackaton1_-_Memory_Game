5b63b1c218c2362d92baa48cc3b5aa6b1307461b
gkimelblat <gabrielkni@gmail.com>
2020-07-01 10:20:19 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# DI_Hackaton1_-_Memory_Game
-Memory Game for my first Hackaton at the Developers.Institute
+# sm-memgame
+Memory Game with Sailor Moon theme.
+
+Ver 0.1 Release Notes:
+
+Game is completely functional already as a simple Memory Game.
---
+++ main.js
@@ -0,0 +1,97 @@
+/* The constant "cardBoard" is a "document.querySelector", which have the function of returning a STATIC NodeList 
+representing a list (array) of documments that match the designated selectors.
+The constant "images" is the array containing all the images to be called by the constant "cardBoard".
+*/
+const cardBoard = document.querySelector('#cardboard');
+const images = [
+    '00_fool.jpg',
+    '01_magician.jpg',
+    '02_priestess.jpg',
+    '03_empress.jpg',
+    '04_emperor.jpg',
+    '05_hierophant.jpg',
+    '06_love.jpg',
+    '07_chariot.jpg',
+    '08_strength.jpg',
+    '09_hermit.jpg',
+];
+/* The variable cardHTML represents the div class 'memory_card', which refers to 'data-card' as the Data Set of images in the folder 'img'; 
+the img classes for frontFace and backFace, which allows to control both sides of the cards with the designated images.
+*/
+let cardHTML = '';
+
+images.forEach(img => {
+    cardHTML += `
+    <div class='memory_card' data-card='${img}'>
+        <img class='frontFace' src='img/${img}'>
+        <img class='backFace' src='img/cardBack.jpg'>
+    </div>
+    `
+});
+
+// This command will make each card be rendered twice, so we can have matching cards.
+cardBoard.innerHTML = cardHTML + cardHTML;
+// End of HTML RENDER
+
+
+/* The constant 'cards' have a 'querySelectorAll' for the div class 'memory_card' that returns a static NodeList 
+of the elements of the div class specified, so we can have our matching cards. 
+The variables 'firstCard' and 'secondCard' represent the first clicked card and the second clicked card.
+The boolean variable lockCard ensures that only two cards can be active at a time, so when the user opens the first card it will stay open,
+and when the user opens the second card, the fuction 'checkForMatch' will check if they are correct.
+The function 'flipCard' switches the div class to make our cards flip sides.
+If the cards are correct, both will stay open, and the fucntion 'resetCards' will remove them from the list of the clickable cards.
+If they are not a match, the function 'disableCards' wil let them open for a 1000ms (1 second) and then close both.
+ */
+
+const cards = document.querySelectorAll('.memory_card');
+let firstCard, secondCard;
+let lockCard = false;
+
+
+function flipCard(){
+    if (lockCard) return false;
+    this.classList.add('flip');
+
+    if(!firstCard){
+        firstCard = this;
+        
+        return false;
+    } 
+        
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch(){
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+    !isMatch ? disableCards() : resetCards(isMatch)}
+
+function disableCards(){
+    lockCard = true;
+    setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetCards();
+    }, 1000);
+}
+
+(function shuffle(){
+    cards.forEach(card => {
+        let rand = Math.floor(Math.random() * 20);
+        card.style.order = rand;
+    });
+})();
+
+function resetCards(isMatch = false){
+    if(isMatch){
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+    }
+    [firstCard, secondCard, lockCard] = [null, null, false];
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
